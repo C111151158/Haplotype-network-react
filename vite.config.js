c13296bdedd53af81a67c8b8112e27c4c906c13a
C111151158 <c111151158@nkust.edu.tw>
2025-05-14 02:22:13 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: ' https://c111151158.github.io',
+        target: ' http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
