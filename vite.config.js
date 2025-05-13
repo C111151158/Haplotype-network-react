864fa54147170c377bfa28fd36d2e0335800b23d
C111151158 <c111151158@nkust.edu.tw>
2025-05-14 02:02:03 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-   base: "/Haplotype-network/",
+  base: "/Haplotype-network/", // ✅ 確保這跟 GitHub Repo 名一致
+
   plugins: [react()],
+
   worker: {
     format: "es",
-    plugins: [react()], 
+    plugins: () => [react()], // ✅ 改成 function 回傳陣列
   },
 
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // 你的後端
+        target: 'http://localhost:3000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // 把 /api 去掉
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
-
 });
-
-
